1b083faa02720f8d8e15a7dae494d98bd9096cee
Hayley <bra22025@byui.edu>
2025-11-05 18:02:18 -0700

    code cleanup, fixed some issues, started user profiles

--- public/verify.js
+++ public/verify.js
@@ -1,9 +1,4 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
-
-const supabaseUrl = 'https://zanpecuhaoukjvjkvyxh.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InphbnBlY3VoYW91a2p2amt2eXhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3NTQ2NjcsImV4cCI6MjA3NDMzMDY2N30.vEu1tr9yYv-eAl6jB6oKHJmGVa70H-OBcTfGhfvcws0';
-
-const supabase = createClient(supabaseUrl, supabaseKey);
+import { supabase } from './supabaseclient.js';
 
 async function sendOtp() {
     console.log("Sending OTP...");
@@ -32,7 +27,7 @@ async function verifyOtp() {
     const { data, error } = await supabase.auth.verifyOtp({
         email,
         token,
-        type: "magiclink" // or 'email' depending on your setup
+        type: "email"
     });
 
     if (error) {
