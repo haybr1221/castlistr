aa1bd284b15787717f542dc49d931003409af4ee
Hayley <hbranchflower@gmail.com>
2026-09-10 11:25:25 -0600

    chore: fix get started
    Make sure to check if a user is logged in already or not before redirecting to home or sign in page

--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -1,12 +1,26 @@
+import { useCurrentUser } from '../config/currentUser.js'
 import { Link } from 'react-router-dom'
 
 function IndexPage() {
+    const { user } = useCurrentUser()
+
+    function handleGetStarted() {
+        if (user) {
+            // no need to sign in, take right to home
+            window.location.href = '/home'
+        }
+
+        else {
+            // take to sign in page
+            window.location.href = '/signin'
+        }
+    }
     return (
         <main className="center-content">
             <div id="main-container">
                 <h1 id="title">castlistr</h1>
                 <p className="text" id="tagline">Fancasting, made simple.</p>
-                <Link to="/signin"> <button className="button">Get Started</button></Link>
+                <button className="button" onClick={handleGetStarted}>Get Started</button>
             </div>
         </main>
     )
